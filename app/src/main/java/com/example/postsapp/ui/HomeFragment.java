3d29f7d442831d61fc2673ecf060a80f005e55e1
Dina <dinailkady@gmail.com>
2022-10-21 21:08:21 +0200

    applay navigation

--- app/src/main/java/com/example/postsapp/ui/HomeFragment.java
+++ app/src/main/java/com/example/postsapp/ui/HomeFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 import com.example.postsapp.R;
 
 
-public class postsFragment extends Fragment {
+public class HomeFragment extends Fragment {
 
 
-    public postsFragment() {
+    public HomeFragment() {
         // Required empty public constructor
     }
 
@@ -30,6 +30,6 @@ public class postsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_posts, container, false);
+        return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
